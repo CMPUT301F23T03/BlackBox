02f8a31ac29565e2013c94f236b9609a826348bd
xuanvietchu <vietxuanchu657@gmail.com>
2023-11-01 00:39:39 -0600

    Change build.gradle:app from sdk 33 to 34 | Added ScanFragment, NavigationBar and some pictures

--- app/src/main/java/com/example/blackbox/MainActivity.java
+++ app/src/main/java/com/example/blackbox/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.blackbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        // Set a listener to handle item selection in the BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        if (id == R.id.menu_home){}
+
+                        else if (id == R.id.menu_scan){
+                            // load scanFragment
+                            loadFragment(new ScanFragment());
+                        }
+                        // Add more cases for other menu items if needed
+                        return true;
+                    }
+                }
+        );
+    }
+    private void loadFragment(Fragment fragment) {
+        // Create a FragmentManager from the support library
+        FragmentManager fm = getSupportFragmentManager();
+        // Create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // Replace the FrameLayout with the new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        // Save the changes
+        fragmentTransaction.commit();
     }
 }
