28be4640ed5c9dd14ac98b90ea4fb17c0d93f712
Riley Kinahan <kinahanriley@gmail.com>
2023-11-04 21:55:07 -0600

    Tag list working

--- app/src/main/java/com/example/blackbox/Tag.java
+++ app/src/main/java/com/example/blackbox/Tag.java
@@ -1,4 +1,25 @@
 package com.example.blackbox;
 
+import java.util.ArrayList;
+
 public class Tag {
+    private String name;
+    private String color;
+    /**
+     * Constructor of an Tag object
+     *
+     * @param  name  the name of the tag
+     */
+    public Tag(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
--- app/src/main/java/com/example/blackbox/TagFragment.java
+++ app/src/main/java/com/example/blackbox/TagFragment.java
@@ -1,21 +1,31 @@
 package com.example.blackbox;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 /**
  * A fragment responsible for viewing, editing, and adding tags
  */
 public class TagFragment extends Fragment {
-    View view;
+    private ArrayList<Tag> tagList;
+    private ListView tagListView;
+    private ArrayAdapter<Tag> tagAdapter;
+    private View view;
+    private Context activityContext;       // context of MainActivity
+
     /**
      * Called to create the view for the fragment.
      *
@@ -41,5 +51,35 @@ public class TagFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // initialize database
+        tagList = new ArrayList<>();
+        tagListView = view.findViewById(R.id.tag_view);
+        tagAdapter = new TagAdapter(activityContext, tagList);
+        tagListView.setAdapter(tagAdapter);
+
+        tagList.add(new Tag("Tag 1", "#FF5733"));
+        tagList.add(new Tag("Tag 2", "#FF1230"));
+        tagAdapter.notifyDataSetChanged();
     }
+    /**
+     * Called when the fragment is attached to an activity.
+     *
+     * @param context The context of the activity to which the fragment is attached.
+     */
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activityContext = context;
+    }
+
+    /**
+     * Called when the fragment is detached from the activity.
+     */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activityContext = null;
+    }
+
 }
