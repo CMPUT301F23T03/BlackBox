2af1e4b57c340dd99489b4d50e6f802f1cbab1f9
Riley Kinahan <kinahanriley@gmail.com>
2023-11-04 22:59:30 -0600

    Connected Tags to DB

--- app/src/test/java/com/example/blackbox/TagFragmentTest.java
+++ app/src/test/java/com/example/blackbox/TagFragmentTest.java
@@ -1,5 +1,10 @@
 package com.example.blackbox;
 
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.junit.Test;
+
 public class TagFragmentTest {
 
 }
