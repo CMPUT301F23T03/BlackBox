998b4194acfc60eb28584b1a03d4918ff109576e
xuanvietchu <vietxuanchu657@gmail.com>
2023-11-07 10:51:14 -0700

    Add gallery detection

--- app/src/main/java/com/example/blackbox/ScanGalleryFragment.java
+++ app/src/main/java/com/example/blackbox/ScanGalleryFragment.java
@@ -4,16 +4,23 @@ package com.example.blackbox;
 import static android.Manifest.permission.READ_MEDIA_IMAGES;
 import static android.Manifest.permission.READ_MEDIA_VIDEO;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
@@ -22,18 +29,50 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class ScanGalleryFragment extends Fragment {
     private static final String READ_MEDIA_IMAGES_PERMISSION = READ_MEDIA_IMAGES;
+    private BarcodeDetector barcodeDetector;
+    private TextView barcodeText;
+    private String barcodeData;
     private ImageView imageView;
     private Button choosePictureButton;
 
+    @SuppressLint("RestrictedApi")
     ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
             registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
                 if (uri != null) {
                     imageView.setImageURI(uri);
                     Log.d("PhotoPicker", "Selected URI: " + uri);
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+                    if(barcodes != null && barcodes.size() > 0){
+                        for (int i = 0; i < barcodes.size(); i++){
+                            Log.d(LOG_TAG, "Value: " + barcodes.valueAt(i).rawValue + "----" + barcodes.valueAt(i).displayValue);
+                            Toast.makeText(requireContext(), barcodes.valueAt(i).rawValue, Toast.LENGTH_SHORT).show();
+                        }
+                        barcodeData = barcodes.valueAt(0).displayValue;
+                        barcodeText.setText(barcodeData);
+                    }else {
+                        Log.e(LOG_TAG,"SparseArray null or empty");
+                        Toast.makeText(requireContext(), "Barcode not found", Toast.LENGTH_SHORT).show();
+                        barcodeText.setText("Barcode not found");
+                    }
+
                 } else {
                     Log.d("PhotoPicker", "No media selected");
                 }
@@ -45,6 +84,10 @@ public class ScanGalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.gallery_scan_fragment, container, false);
         imageView = view.findViewById(R.id.image_view);
+        barcodeDetector = new BarcodeDetector.Builder(requireContext())
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        barcodeText = view.findViewById(R.id.barcode_text);
 
         choosePictureButton = view.findViewById(R.id.choose_button);
         choosePictureButton.setOnClickListener(v -> {
@@ -67,6 +110,8 @@ public class ScanGalleryFragment extends Fragment {
             EasyPermissions.requestPermissions(this,"Please grant permission",
                    REQUEST_GALLERY_PERMISSION, perms);
             Toast.makeText(requireContext(), "Please grant permission to access the gallery", Toast.LENGTH_SHORT).show();
+            pickMedia.launch(new PickVisualMediaRequest());
+
         }
     }
 
