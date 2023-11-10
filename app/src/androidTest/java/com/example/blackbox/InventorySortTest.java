c610a3ae6058d25d3ba9e97b3049a38e91a9ff25
SheikhSH <sakifhossain71@gmail.com>
2023-11-11 04:01:08 +0600

    Update InventorySortTest.java

--- app/src/androidTest/java/com/example/blackbox/InventorySortTest.java
+++ app/src/androidTest/java/com/example/blackbox/InventorySortTest.java
@@ -1,5 +1,6 @@
 package com.example.blackbox;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
@@ -27,55 +28,42 @@ import org.junit.Rule;
 import org.junit.Test;
 
 public class InventorySortTest {
-
-    /**
-     * Method used to clear all DBs for testing
-     */
-    private void clearDBs(){
-        // clear inventory first to avoid errors
-        InventoryDBTest.clearInventoryDB();
-        TagDBTest.clearTagDB();
-    }
-
     // initialize main activity
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
     public void setup(){
-        clearDBs();
-
-        //Mock data for testing
-        //click on add button
+        // start with a fresh database
+        InventoryDBTest.clearInventoryDB();
+        // click on addItem
         onView(withId(R.id.add_button)).perform(click());
-        //input name
-        onView(withId(R.id.name_editText)).perform(ViewActions.replaceText("Car"));
-        //input value
-        onView(withId(R.id.value_editText)).perform(ViewActions.replaceText("100"));
-
-
+        // fill information
+        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("Random Test"));
+        onView(withId(R.id.value_editText)).perform(ViewActions.replaceText(String.valueOf(150)));
+        // Press add
+        onView(withId(R.id.small_save_button)).perform(click());
+        // click on addItem
+        onView(withId(R.id.add_button)).perform(click());
+        // fill information
+        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("test 2"));
+        onView(withId(R.id.value_editText)).perform(ViewActions.replaceText(String.valueOf(100)));
+        // Press add
+        onView(withId(R.id.small_save_button)).perform(click());
     }
 
-
     @Test
-    public void testSortByDate(){
+    public void testSortByValue(){
+        setup();
         // Click on the "Sort" button
         onView(withId(R.id.sort_button)).perform(click());
         // Choose sort by date
         onView(withId(R.id.sort_category_spinner)).perform(click());
-        Espresso.onData(allOf(is(instanceOf(String.class)), is("By Date")))
-                .perform(click());
+        onView(withText("By Value")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
         //Choose sort order as ascending
         onView(withId(R.id.sort_order_spinner)).perform(click());
-        Espresso.onData(allOf(is(instanceOf(String.class)), is("Ascending")))
-                .perform(click());
+        onView(withText("Ascending")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
         //Click OK
-        onView(withText("OK"))
-                .perform(click());
-
-        onView(allOf(isAssignableFrom(TextView.class), withId(R.id.name)))
-                .check(matches(withText("item1")));
-
-
-
+        onView(withText("OK")).perform(click());
+        onView(allOf(isAssignableFrom(TextView.class), withId(R.id.name))).check(matches(withText("test 2")));
     }
 }
