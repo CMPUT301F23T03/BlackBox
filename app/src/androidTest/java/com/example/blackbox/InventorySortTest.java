41e41ba82aa71febb2f73cc3f2fd019b7fa8c276
Andy Tran <anhquoctran006@gmail.com>
2023-11-10 15:41:36 -0700

    Finalized UI testing for sorting

--- app/src/androidTest/java/com/example/blackbox/InventorySortTest.java
+++ app/src/androidTest/java/com/example/blackbox/InventorySortTest.java
@@ -4,6 +4,8 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -14,6 +16,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 import static java.util.regex.Pattern.matches;
 
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.test.espresso.Espresso;
@@ -24,6 +27,7 @@ import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -32,38 +36,56 @@ public class InventorySortTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    public void setup(){
-        // start with a fresh database
-        InventoryDBTest.clearInventoryDB();
-        // click on addItem
+    /**
+     * Adds item 1
+     */
+    public void addTestItem1() {
         onView(withId(R.id.add_button)).perform(click());
-        // fill information
-        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("Random Test"));
+        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("Item 1"));
         onView(withId(R.id.value_editText)).perform(ViewActions.replaceText(String.valueOf(150)));
-        // Press add
         onView(withId(R.id.small_save_button)).perform(click());
-        // click on addItem
+        try {
+            Thread.sleep(1000); // Add a 1-second delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Adds item 2
+     */
+    public void addTestItem2() {
         onView(withId(R.id.add_button)).perform(click());
-        // fill information
-        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("test 2"));
+        onView(withId(R.id.name_editText)).perform(ViewActions.typeText("Item 2"));
         onView(withId(R.id.value_editText)).perform(ViewActions.replaceText(String.valueOf(100)));
-        // Press add
         onView(withId(R.id.small_save_button)).perform(click());
     }
 
+    /**
+     * This tests that sorting by value and ascending works
+     */
     @Test
     public void testSortByValue(){
-        setup();
+        InventoryDBTest.clearInventoryDB();
+        addTestItem1();
+        addTestItem2();
         // Click on the "Sort" button
         onView(withId(R.id.sort_button)).perform(click());
         // Choose sort by date
         onView(withId(R.id.sort_category_spinner)).perform(click());
         onView(withText("By Value")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        //Choose sort order as ascending
+        // Choose sort order as ascending
         onView(withId(R.id.sort_order_spinner)).perform(click());
         onView(withText("Ascending")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        //Click OK
+        // Click OK
         onView(withText("OK")).perform(click());
-        onView(allOf(isAssignableFrom(TextView.class), withId(R.id.name))).check(matches(withText("test 2")));
+        try {
+            Thread.sleep(1000); // Add a 1-second delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // check the sorted order
+        onData(is(instanceOf(Item.class))).inAdapterView(withId(R.id.item_list)).atPosition(0).perform(click());
+        onView(withText("Item 2")).check(matches(isDisplayed()));
     }
 }
