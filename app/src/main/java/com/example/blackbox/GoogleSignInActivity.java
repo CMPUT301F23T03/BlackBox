59a4018c43b4cacddf476abe43a4d62f88219884
Andy Tran <anhquoctran006@gmail.com>
2023-11-14 14:16:05 -0700

    Created a new activity for sign-in

---
+++ app/src/main/java/com/example/blackbox/GoogleSignInActivity.java
@@ -0,0 +1,52 @@
+package com.example.blackbox;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    /**
+     * Called when the activity is first initialized
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // FirebaseUser currentUser = mAuth.getCurrentUser();
+        // updateUI(currentUser);
+    }
+
+    /**
+     * Called when the activity is created. .
+     *
+     * @param savedInstanceState A Bundle containing the saved state of the activity.
+     */
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState,
+                         @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+        // ...
+}
