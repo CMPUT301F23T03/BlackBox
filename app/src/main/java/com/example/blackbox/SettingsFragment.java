57f16323718a096b7058519ea270d7202c03eedf
Riley <kinahanriley@gmail.com>
2023-11-25 23:59:18 -0700

    Added language spinner

--- app/src/main/java/com/example/blackbox/SettingsFragment.java
+++ app/src/main/java/com/example/blackbox/SettingsFragment.java
@@ -1,26 +1,51 @@
 package com.example.blackbox;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.blackbox.authentication.GoogleAuthDB;
+import com.example.blackbox.authentication.GoogleSignInActivity;
 import com.example.blackbox.profile.ProfileFragment;
 import com.example.blackbox.tag.TagFragment;
 import com.example.blackbox.utils.NavigationManager;
 
+import java.util.ArrayList;
+
 public class SettingsFragment extends Fragment {
     View view;
     View tagsLayout;
+    View logOutLayout;
+    Spinner langSpinner;
+    ArrayAdapter<String> langSpinAdapter;
     ImageButton profilePicture;
+    Context activityContext;
+    String selectedLanguage;
     private GoogleAuthDB googleAuthDB = new GoogleAuthDB();
 
+
+    /**
+     * Called when the fragment is attached to an activity.
+     *
+     * @param context The context of the activity to which the fragment is attached.
+     */
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activityContext = context;
+    }
 
     /**
      * Called to create the view for the fragment
@@ -62,6 +87,28 @@ public class SettingsFragment extends Fragment {
         });
         profilePicture.setOnClickListener(v -> {
             NavigationManager.switchFragmentWithoutBack(new ProfileFragment(), getParentFragmentManager());
+        });
+        logOutLayout = view.findViewById(R.id.logout_cl);
+        logOutLayout.setOnClickListener(v -> {
+            googleAuthDB.logOut();
+            Intent i = new Intent(activityContext, GoogleSignInActivity.class);
+            startActivity(i);
+        });
+        langSpinner = view.findViewById(R.id.language_spinner);
+        final String[] languages = {"English"};
+        langSpinAdapter = new ArrayAdapter<String>(activityContext, android.R.layout.simple_spinner_item,languages);
+        langSpinner.setAdapter(langSpinAdapter);
+        langSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Handle the selected item here
+                selectedLanguage = langSpinAdapter.getItem(position);
+                Log.d("onItemSelected", "Selected: " + selectedLanguage.toString());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing when nothing is selected
+            }
         });
     }
 }
